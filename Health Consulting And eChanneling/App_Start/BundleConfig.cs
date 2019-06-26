96c58f6eab766bf1349873fdc42e2f06c2ec5919
dspdamith <dspdamithse@gmail.com>
2019-06-26 11:03:01 +0530

    Home Interface Change with Template

--- Health Consulting And eChanneling/App_Start/BundleConfig.cs
+++ Health Consulting And eChanneling/App_Start/BundleConfig.cs
@@ -29,6 +29,35 @@ namespace Health_Consulting_And_eChanneling
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+
+
+
+            //Website Bundle Configularation
+            bundles.Add(new StyleBundle("~/Content/healhConsultCSS").Include(
+                      "~/Content/HealthConsultAllCSS/lib/bootstrap/css/bootstrap.min.css",
+                      "~/Content/HealthConsultAllCSS/lib/font-awesome/css/font-awesome.min.css",
+                      "~/Content/HealthConsultAllCSS/lib/animate/animate.min.css",
+                      "~/Content/HealthConsultAllCSS/lib/ionicons/css/ionicons.min.css",
+                      "~/Content/HealthConsultAllCSS/lib/owlcarousel/assets/owl.carousel.min.css",
+                      "~/Content/HealthConsultAllCSS/lib/lightbox/css/lightbox.min.css",
+                      "~/Content/HealthConsultAllCSS/css/style.css"
+                      ));
+
+            bundles.Add(new ScriptBundle("~/bundles/healhConsultJs").Include(
+                      "~/Content/HealthConsultAllCSS/lib/jquery/jquery.min.js",
+                      "~/Content/HealthConsultAllCSS/lib/bootstrap/js/bootstrap.bundle.min.js",
+                      "~/Content/HealthConsultAllCSS/lib/easing/easing.min.js",
+                      "~/Content/HealthConsultAllCSS/lib/mobile-nav/mobile-nav.js",
+                      "~/Content/HealthConsultAllCSS/lib/wow/wow.min.js",
+                      "~/Content/HealthConsultAllCSS/lib/waypoints/waypoints.min.js",
+                      "~/Content/HealthConsultAllCSS/lib/counterup/counterup.min.js",
+                      "~/Content/HealthConsultAllCSS/lib/owlcarousel/owl.carousel.min.js",
+                      "~/Content/HealthConsultAllCSS/lib/isotope/isotope.pkgd.min.js",
+                      "~/Content/HealthConsultAllCSS/lib/lightbox/js/lightbox.min.js",
+                      "~/Content/HealthConsultAllCSS/contactform/contactform.js",
+                      "~/Content/HealthConsultAllCSS/js/main.js"
+                      ));
         }
     }
 }

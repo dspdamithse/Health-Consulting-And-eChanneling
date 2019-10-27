b5654dffc40dd2d78463f73592739531734ad99c
dspdamith <dspdamithse@gmail.com>
2019-10-27 14:55:05 +0530

    Medical Records Upload and View Complete

--- Health Consulting And eChanneling/Areas/Administrator/Controllers/UsersController.cs
+++ Health Consulting And eChanneling/Areas/Administrator/Controllers/UsersController.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Mvc;
 
 namespace Health_Consulting_And_eChanneling.Areas.Administrator.Controllers
@@ -22,7 +23,7 @@ namespace Health_Consulting_And_eChanneling.Areas.Administrator.Controllers
 
             using (Db db = new Db())
             {
-                userList = db.Users.ToArray().OrderByDescending(x => x.Id).Where(x=>x.RoleConfirm== 0).Where(x=>x.Id != 1).Select(x => new UserViewModel(x)).ToList();
+                userList = db.Users.ToArray().OrderByDescending(x => x.Id).Where(x => x.RoleConfirm == 0).Where(x => x.Id != 1).Select(x => new UserViewModel(x)).ToList();
             }
 
             return View(userList);
@@ -41,10 +42,39 @@ namespace Health_Consulting_And_eChanneling.Areas.Administrator.Controllers
                 }
                 model = new UserProfileViewModel(dto);
 
-                model.MedicalImages = Directory.EnumerateFiles(Server.MapPath("~/Content/MedicalImages/"+id+"/"))
+                model.MedicalImages = Directory.EnumerateFiles(Server.MapPath("~/Content/MedicalImages/" + id + "/"))
                                                .Select(fn => Path.GetFileName(fn));
             }
             return View(model);
+        }
+        [HttpPost]
+        public void SaveMedicalImages(int id)
+        {
+            foreach (string fileName in Request.Files)
+            {
+                HttpPostedFileBase file = Request.Files[fileName];
+
+                if (file != null && file.ContentLength > 0)
+                {
+                    var originalDirectory = new DirectoryInfo(string.Format("{0}Content\\MedicalImages", Server.MapPath(@"\")));
+
+                    string pathString = Path.Combine(originalDirectory.ToString(), id.ToString());
+
+                    var path = string.Format("{0}\\{1}", pathString, file.FileName);
+
+
+                    file.SaveAs(path);
+                }
+            }   
+        }
+
+        public void DeleteImage(int id, string imageName)
+        {
+            string fullpath1 = Request.MapPath("~/Content/MedicalImages" + id.ToString() +"/"+ imageName);
+
+            if (System.IO.File.Exists(fullpath1))
+                System.IO.File.Delete(fullpath1);
+
         }
     }
 }
